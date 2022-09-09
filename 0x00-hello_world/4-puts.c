fa92bab357efb13a88b319fee80ca602b6a02302
lacxis <allenprovidence@yahoo.com>
2022-09-08 18:16:05 -0700

    a C program that prints using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-	/**this is where the code begins
+	/**main - Prints out  "Programming is like building a multilingual puzzle
 	*first thing is to create a  string and put it in a char container
+	*
 	*put the string in Mystr to the screen
-	**/
-
+	*/
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
