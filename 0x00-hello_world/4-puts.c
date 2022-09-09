67ca65ecea364b75db9b2be8fbccb1610de7591c
lacxis <allenprovidence@yahoo.com>
2022-09-08 17:21:25 -0700

    writing a C program using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main()
+	int main(void)/*this is where the code begins*/
 {
-        /*first thing is to create a  string and put it in a char container*/
+	/*first thing is to create a  string and put it in a char container*/
 
-    char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
 
-    puts(Mystr);  /*put the string in Mystr to the screen*/
+	puts(Mystr);  /*put the string in Mystr to the screen*/
 
-    return 0;
+    return (0);
 }
