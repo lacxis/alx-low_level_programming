80ce86a5314da8f5c4176222956b001977f156e7
lacxis <allenprovidence@yahoo.com>
2022-09-09 03:07:31 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	printf("char size: %lu bytes\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-	printf("int size: %lu bytes\n", sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-	printf("long size: %lu bytes\n", sizeof(long));
+	printf("Size of a long: %d byte(s)\n", sizeof(long int));
 
-	printf("long long: %lu bytes\n", sizeof(long long));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long int));
 
-	printf("float size: %lu bytes\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 return (0);
 }
