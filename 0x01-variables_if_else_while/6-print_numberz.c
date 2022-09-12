4c156987b5e3c1116d54019544939746232a0c3c
lacxis <allenprovidence@yahoo.com>
2022-09-11 19:09:10 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all  base 10 digit,
+ * Return: 0 on SUCCESS
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
