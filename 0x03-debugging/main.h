ce298b4d41df80eaa50b4e9fa541fa64a9fac2ce
lacxis <allenprovidence@yahoo.com>
2022-09-15 02:06:11 -0700

    3

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef ALX_SCHOOL_H
+#define ALX_SCHOOL_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* ALX_SCHOOL_H */
