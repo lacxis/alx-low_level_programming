73a6fc58fcadb9dd0b9f217c96710a5498b2e8bd
lacxis <allenprovidence@yahoo.com>
2023-01-16 14:08:39 +0100

    list numbers from 0-9 followed by new line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* lists base 10 numbers 0-9 followed by newline using putchar */
 
 /**
  * main - lists numbers and exits
+ *
+ * Description - A program that lists base 10 numbers(0-9) followed by newline
+ * using putchar using function
+ *
  * Return: returns 0
  */
+
 int main(void)
 {
 	int n;
 
-	n = '0';
-	while (n <= '9')
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		n++;
 	}
 
 	putchar('\n');
