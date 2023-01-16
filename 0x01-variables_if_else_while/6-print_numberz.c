528806d715802b5879bfe074063ece21fac10118
lacxis <allenprovidence@yahoo.com>
2023-01-16 14:15:15 +0100

    prints out single digits of base with using int

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints all  base 10 digit,
+ * main - prints all  base 10 digit
+ *
+ * Description - A program that prints out single digits numbers of base 10
+ * without a variable type character used
+ *
  * Return: 0 on SUCCESS
  */
 
@@ -9,7 +13,11 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
+	{
 		putchar(n);
+	}
+
 	putchar('\n');
+
 	return (0);
 }
