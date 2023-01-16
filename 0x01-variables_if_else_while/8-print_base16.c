88272a0f9fbb1d736bcd8aefb3a9d3adaddc09e5
lacxis <allenprovidence@yahoo.com>
2023-01-16 14:33:30 +0100

    print out hexadecimal value

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+
 /**
- *main - Print hexadecimal value,
- *Return: 0 on SUCCESS
+ * main - Print hexadecimal value
+ *
+ * Description - A program that prints all the numbers of base 16 in lowercase
+ * followed by a new line
+ *
+ * Return: return 0 on success
  */
+
 int main(void)
 {
-	int i;
+	char i;
 	char chr;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9'; i++)
+	{
 		putchar(i);
-	for (chr = 'a'; chr < 'g'; chr++)
+	}
+
+	for (chr = 'a'; chr <= 'f'; chr++)
+	{
 		putchar(chr);
+	}
+
 	putchar('\n');
 	return (0);
 }
